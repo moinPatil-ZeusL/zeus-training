2cf9b36b88002c19e7c056be237813a3a979ee30
moin <moin.patil@zeuslearning.com>
2023-07-12 15:18:13 +0530

    task1 10 July 2023

---
+++ 100723 task/script.js
@@ -0,0 +1,15 @@
+console.log('succ');
+
+const form = document.getElementById('form');
+const name1 = document.getElementById('name1');
+const comments = document.getElementById('comments');
+
+checkForm = () => {
+    alert(name1.value);    
+}
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     alert('Submitted Successfully');
+// })
+
